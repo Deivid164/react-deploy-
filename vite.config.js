aafd250724c96ad3d612fb709067de9482076758
deivid164 <dgonzalezm6@gmail.com>
2024-12-18 21:55:10 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://Deivid164.github.io/react-deploy-example/'
-})
+  base: "/react-deploy-"
+});
